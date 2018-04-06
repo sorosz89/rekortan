9d10b29f978796fdb15384079f19b4f13da0d64e
sorosz89 <os.sandor@gmail.com>
2018-04-06 22:17:15 +0200

    Update hooks.js

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -10,14 +10,11 @@ global.expect = chai.expect;
 var webdriver = require('selenium-webdriver');
 global.by = webdriver.By;
 
-module.exports = function(){
+module.exports = () => {
     this.setDefaultTimeout(60000);
-    this.registerHandler('BeforeFeatures', function(){
-        global.driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
-        return global.driver.manage().window().maximize();
+    this.registerHandler('BeforeFeatures', () => 
+      return new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
     });
 
-    this.registerHandler('AfterFeatures', function(){
-        return global.driver.quit();
-    });
+    this.registerHandler('AfterFeatures', () => global.driver.quit());
 };
